9e53a2accbcc25adaff35933d0b8a14aa83e5d96
wan0124 <jcps3503@gmail.com>
2017-03-15 01:34:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ data=0
 
 pi=pigpio.pi()  
 if not pi.connected:
- exit(0)
+  exit(0)
     
 pi.set_mode(MCLK_PIN,pigpio.OUTPUT)
 pi.set_mode(SCLK_PIN,pigpio.OUTPUT)
@@ -21,43 +21,43 @@ pi.set_mode(LRCLK_PIN,pigpio.OUTPUT)
 pi.set_mode(DATA_PIN,pigpio.OUTPUT)
 
 def MCLK():
- while TRUE :
-  pi.write(MCLK_PIN,pulse)
-  pulse=~pulse
-  count += 1
-  time.sleep(1/(2*f))
+  while TRUE :
+    pi.write(MCLK_PIN,pulse)
+    pulse=~pulse
+    count += 1
+    time.sleep(1/(2*f))
   
 def SCLK():
- while TRUE :
-  prepulse=pulse
+  while TRUE :
+    prepulse=pulse
    
   if pulse :
-   pi.write(SCLK_PIN,1)
+    pi.write(SCLK_PIN,1)
   else :
-   pi.write(SCLK_PIN,0)
+    pi.write(SCLK_PIN,0)
   while pulse==prepulse:
-   pass
+    pass
 
 def LRCLK():
- pi.write(LRCLK_PIN,0)
- while TRUE :
-  prepulse=pulse
+  pi.write(LRCLK_PIN,0)
+  while TRUE :
+    prepulse=pulse
   if count == 16 :
-   pi.write(LRCLK_PIN,1)
+    pi.write(LRCLK_PIN,1)
   elif count == 32:
-   pi.write(LRCLK_PIN,0)
-   count=1
+    pi.write(LRCLK_PIN,0)
+    count=1
   while pulse==prepulse:
-   pass
+    pass
    
 def DATA():
   while TRUE :
-   while pulse==0:
-    pass
-   while pulse==1: 
-    pass
-   pi.write(DATA_PIN,data)
-   data=~data
+    while pulse==0:
+      pass
+    while pulse==1: 
+      pass
+    pi.write(DATA_PIN,data)
+    data=~data
      
 thread.start_new_thread (MCLK())
 thread.start_new_thread (SCLK())
