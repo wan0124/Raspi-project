bf296d22948dde835b0882c43d26011d1731d344
wan0124 <jcps3503@gmail.com>
2017-03-14 20:14:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import thread
+import time
 import RPi.GPIO as GPIO
 
-MCLK_PIN=
-SCLK_PIN=
-LRCLK_PIN=
-DATA_PIN=
+MCLK_PIN=11
+SCLK_PIN=12
+LRCLK_PIN=15
+DATA_PIN=16
+f=44100
+
 
 def MCLK():
   
@@ -13,3 +16,8 @@ def SCLK
 def LRCLK():
   
 def DATA():
+
+thread.start_new_thread (MCLK)
+thread.start_new_thread (SCLK)
+thread.start_new_thread (LRCLK)
+thread.start_new_thread (DATA)
